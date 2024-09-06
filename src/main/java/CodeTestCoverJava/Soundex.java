5f574fa3e18e744037f173d39ddd0a8bccda31aa
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-09-06 22:44:48 +0530

    handled edge cases

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Soundex {
 	
 	static Map<Character, Character> soundexCodes = new HashMap<>();
-	
+	static List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o','u');
+
 	private static void initSoundexCodes() {
 		soundexCodes.put('B', '1');
 		soundexCodes.put('F', '1');
@@ -31,8 +32,9 @@ public class Soundex {
         if (name == null || name.isEmpty()) {
             return "";
         }
-	initSoundexCodes();
+        initSoundexCodes();
         StringBuilder soundex = new StringBuilder();
+        name = name.replaceAll("[^a-zA-Z0-9]", "");
 		appendInitialCharacter(soundex, name);
         processRemainingCharacters(soundex, name);
 		checkAndAddPadding(soundex);
@@ -48,22 +50,32 @@ public class Soundex {
 	
 	// Processes and appends the remaining characters based on Soundex encoding
 	private static void processRemainingCharacters(StringBuilder soundex, String name) {
-		char prevCode = getSoundexCode(name.charAt(0));
+		
+        char prevCode = getSoundexCode(name.charAt(0));
 		 for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
+		     // [a,e,i,o,u]
 			char currentChar = name.charAt(i);
 			char currentCode = getSoundexCode(currentChar);
-			updateSoundex(soundex, currentCode, prevCode);
+			prevCode = updateSoundex(soundex, currentCode, prevCode, checkVowels(name, i));
 		 }
-
-		
+	}
+	
+	private static boolean checkVowels(String name, int index) {
+	    if (index > 1) {
+	        char firstChar = Character.toLowerCase(name.charAt(index-1));
+	        char secondChar = Character.toLowerCase(name.charAt(index-2));
+	        return vowels.contains(firstChar) && vowels.contains(secondChar);
+	    }
+	    return false;
 	}
 
-    private static void updateSoundex(StringBuilder soundex, char currentCode, char prevCode) {
+    private static char updateSoundex(StringBuilder soundex, char currentCode, char prevCode, boolean isVowels) {
 		// Append the code only if it differs from the previous code and is not '0'
-			if (currentCode != '0' && currentCode != prevCode) {
+			if (currentCode != '0' && currentCode != prevCode || isVowels) {
 				soundex.append(currentCode);
 				prevCode = currentCode;
 			}
+			return prevCode;
 	}
 	
 	private static void checkAndAddPadding(StringBuilder soundex) {
@@ -76,4 +88,5 @@ public class Soundex {
         c = Character.toUpperCase(c);
         return soundexCodes.getOrDefault(c, '0');
     }
+    
 }
