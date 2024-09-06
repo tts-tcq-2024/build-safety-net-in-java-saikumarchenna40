00abb50f36b703a55f742c68c047623692f72b74
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-09-06 19:35:31 +0530

    initial commit

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,14 +1,50 @@
 package CodeTestCoverJava;
 
-public class Soundex {
+import java.util.*;
 
+public class Soundex {
+	
+	Map<Character, Character> soundexCodes = new HashMap<>();
+	
+	public Soundex() {
+		initSoundexCodes();
+	}
+
+	private void initSoundexCodes() {
+		soundexCodes.put('B', '1');
+		soundexCodes.put('F', '1');
+		soundexCodes.put('P', '1');
+		soundexCodes.put('V', '1');
+		soundexCodes.put('C', '2');
+		soundexCodes.put('G', '2');
+		soundexCodes.put('J', '2');
+		soundexCodes.put('K', '2');
+		soundexCodes.put('Q', '2');
+		soundexCodes.put('S', '2');
+		soundexCodes.put('X', '2');
+		soundexCodes.put('Z', '2');
+		soundexCodes.put('D', '3');
+		soundexCodes.put('T', '3');
+		soundexCodes.put('L', '4');
+		soundexCodes.put('M', '5');
+		soundexCodes.put('N', '5');
+		soundexCodes.put('R', '6');
+	}
+	
     public static String generateSoundex(String name) {
         if (name == null || name.isEmpty()) {
             return "";
         }
 
         StringBuilder soundex = new StringBuilder();
-        soundex.append(Character.toUpperCase(name.charAt(0)));
+        updateSoundex(soundex);
+
+        return soundex.toString();
+    }
+	
+	private void updateSoundex(StringBuilder soundex) {
+		
+		soundex.append(Character.toUpperCase(name.charAt(0)));
         char prevCode = getSoundexCode(name.charAt(0));
 
         for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
@@ -22,27 +58,11 @@ public class Soundex {
         while (soundex.length() < 4) {
             soundex.append('0');
         }
-
-        return soundex.toString();
-    }
+		
+	}
 
     private static char getSoundexCode(char c) {
         c = Character.toUpperCase(c);
-        switch (c) {
-            case 'B': case 'F': case 'P': case 'V':
-                return '1';
-            case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z':
-                return '2';
-            case 'D': case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M': case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y,z
-        }
+        return soundexCodes.getOrDefault(c, '0');
     }
 }
